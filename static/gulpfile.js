ed9e20be19bb3d4fe6cf8ecfae5235c4478ffde2
Falaleev Maxim <max@studio107.ru>
2014-03-18 19:03:18 +0400

    upload

---
+++ static/gulpfile.js
@@ -0,0 +1,100 @@
+var gulp = require('gulp');
+
+var concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    minifyCSS = require('gulp-minify-css'),
+    sass = require('gulp-sass'),
+    changed = require('gulp-changed'),
+    clean = require('gulp-clean'),
+    cache = require('gulp-cached'),
+    livereload = require('gulp-livereload');
+
+var version = '1.0.0';
+
+var minifyOpts = {
+
+};
+
+var imagesOpts = {
+    optimizationLevel: 5,
+    progressive: true,
+    interlaced: true
+};
+
+var sassOpts = {
+    includePaths: [
+        'vendor/foundation/scss',
+        '/Library/Ruby/Gems/2.0.0/gems/compass-0.12.2/frameworks/compass/stylesheets',
+        'vendor/mindy-sass/mindy'
+    ]
+};
+
+var dst = {
+    js: 'dist/',
+    css: 'dist',
+    images: 'dist/images',
+    sass: './css'
+}
+
+var paths = {
+    js: [
+        'vendor/jquery/dist/jquery.min.js',
+        'vendor/modernizr/modernizr.js',
+        'vendor/jquery.cookie/jquery.cookie.js',
+        'vendor/fastclick/lib/fastclick.js',
+        'vendor/foundation/js/foundation.min.js',
+        'js/**/*.js'
+    ],
+    images: 'images/**/*',
+    sass: 'scss/**/*.scss',
+    css: 'css/**/*.css'
+};
+
+gulp.task('js', function() {
+    return gulp.src(paths.js)
+        .pipe(uglify())
+        .pipe(concat(version + '.all.js'))
+        .pipe(gulp.dest(dst.js));
+});
+
+gulp.task('images', function() {
+    return gulp.src(paths.images)
+        .pipe(changed(dst.images))
+        .pipe(cache('imagemin', imagesOpts))
+        .pipe(gulp.dest(dst.images));
+});
+
+gulp.task('sass', function() {
+    gulp.src(paths.sass)
+        .pipe(sass(sassOpts))
+        .pipe(gulp.dest(dst.sass))
+        .pipe(minifyCSS(minifyOpts))
+        .pipe(concat(version + '.all.css'))
+        .pipe(gulp.dest(dst.css));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+    var server = livereload(),
+        liveReloadCallback = function(file) {
+            setTimeout(function() {
+                server.changed(file.path);
+            }, 300);
+        };
+
+    gulp.watch(paths.js, ['js']).on('change', liveReloadCallback);
+    gulp.watch(paths.images, ['images']);
+    gulp.watch(paths.sass, ['sass']).on('change', liveReloadCallback);
+});
+
+// Clean
+gulp.task('clean', function() {
+    return gulp.src(['dist/*'], {
+        read: false
+    }).pipe(clean());
+});
+
+gulp.task('default', ['clean'], function() {
+    return gulp.start('js', 'sass', 'images');
+});
